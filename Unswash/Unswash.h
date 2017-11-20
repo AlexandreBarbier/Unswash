11dd864d157090132665d2e26fa018860ff128c3
Alexandre <alexandr.barbier@gmail.com>
2017-11-20 14:41:08 +0100

    create project

---
+++ Unswash/Unswash.h
@@ -0,0 +1,19 @@
+//
+//  Unswash.h
+//  Unswash
+//
+//  Created by Alexandre Barbier on 20/11/2017.
+//  Copyright © 2017 Alexandre Barbier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Unswash.
+FOUNDATION_EXPORT double UnswashVersionNumber;
+
+//! Project version string for Unswash.
+FOUNDATION_EXPORT const unsigned char UnswashVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Unswash/PublicHeader.h>
+
+
