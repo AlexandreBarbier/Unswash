045f26b5b33e3f089ce9de3f308922d3618befde
Alexandre <alexandr.barbier@gmail.com>
2017-12-06 10:33:48 +0100

    Build with xc9.2

--- Unswash/PhotosRequest.swift
+++ Unswash/PhotosRequest.swift
@@ -29,9 +29,7 @@ extension Unswash {
                         err.errors = [dataString!]
                         completion([], err)
                     }
-
                 }
-
             }
         }
 
