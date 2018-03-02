3aebc3325a08839fe23717acbfb3e28ba9d6683d
Alexandre <alexandr.barbier@gmail.com>
2018-03-02 14:57:21 +0100

    Fixes

--- Unswash/UI/UnswashPhotoViewController.swift
+++ Unswash/UI/UnswashPhotoViewController.swift
@@ -159,22 +159,24 @@ extension UnswashPhotoViewController : UICollectionViewDataSource, UICollectionV
         cell.index =  indexPath.row
         cell.delegate = self
         cell.dataTask?.cancel()
-
-        if let url = photo.getURLForQuality(quality: imageQuality), imageList[url] == nil {
-            let request = URLRequest(url: URL(string: url)!, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 5.0)
-            cell.dataTask = photoDownloader.dataTask(with: request) { (data, response, error) in
-                guard  let data = data, let img = UIImage(data: data) else {
-                    return
+        if let url = photo.getURLForQuality(quality: imageQuality) {
+            if imageList[url] == nil {
+                let request = URLRequest(url: URL(string: url)!, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 5.0)
+                cell.dataTask = photoDownloader.dataTask(with: request) { (data, response, error) in
+                    guard  let data = data, let img = UIImage(data: data) else {
+                        return
+                    }
+                    self.imageList.updateValue(img, forKey: url)
+                    DispatchQueue.main.async {
+                        cell.imageView.image = img
+                    }
                 }
-                self.imageList.updateValue(img, forKey: url)
-                DispatchQueue.main.async {
-                    cell.imageView.image = img
-                }
+                cell.dataTask.resume()
+                cell.imageView.image = imageList[url]
+            } else {
+                cell.imageView.image = imageList[url]
             }
-            cell.dataTask.resume()
-            cell.imageView.image = imageList[url]
         }
-
         return cell
     }
 }
